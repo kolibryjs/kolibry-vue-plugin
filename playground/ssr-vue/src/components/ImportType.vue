3308e5d89c5f46d244db223135e67b0ef84854df
nyxb <contact@nyxb.xyz>
2023-05-26 16:35:36 +0200

    ✨ feat(playground): add new pages and components to the SSR Vue and Vue playgrounds This commit adds new pages and components to the SSR Vue and Vue playgrounds. The SSR Vue playground now has a new page called About.vue, which displays a message and a URL. It also has a new page called External.vue, which imports an external component. The Home.vue page now has a new component called ImportType.vue, which imports a type and should be removed without side-effect. The Store.vue page now has a new component called Store.vue, which defines a store. The Vue playground now has new components called Assets.vue, AsyncComponent.vue, CssModules.vue, and CustomBlock.vue. These components demonstrate the use of static assets, async components, CSS modules, and custom blocks, respectively.

---
+++ playground/ssr-vue/src/components/ImportType.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { Foo } from '@kolibryjs/dep-import-type-test/deep'
+
+const msg: Foo = {}
+</script>
+
+<template>
+  <p>import type should be removed without side-effect</p>
+</template>
---
+++ playground/ssr-vue/src/pages/External.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable import/no-anonymous-default-export -->
+<script>
+import ExampleExternalComponent from '@kolibryjs/example-external-component-test'
+
+export default {
+   components: {
+      ExampleExternalComponent,
+   },
+}
+</script>
+
+<template>
+  <ExampleExternalComponent />
+</template>
